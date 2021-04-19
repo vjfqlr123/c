0d7aaa9528f9a7b80f98abff88ba586edc67585b
vjfqlr123 <82442705+vjfqlr123@users.noreply.github.com>
2021-04-19 11:09:17 +0900

    Add files via upload

---
+++ rand/rand.c
@@ -0,0 +1,14 @@
+
+static int seed;
+
+void my_srand(int s)			// function definition
+{
+	seed = s;
+}
+
+int my_rand(void)
+{
+	seed = seed * 1103515245 + 12345;
+	
+	return((unsigned)(seed/65536) % 32768);
+}
